d59aab2292b5b74589356eb2d02fdd23c1acfbd4
Dylan Marques <rmapmd1@ucl.ac.uk>
2020-07-22 12:52:27 +0100

    Lightinteraction_recursivegridded works converges as a structures instead of maximum intensity

--- src/lightinteraction_recursivegridded.jl
+++ src/lightinteraction_recursivegridded.jl
@@ -6,7 +6,7 @@ function intensity_p(field::Union{FieldAngularSpectrum{T}, FieldSpace{T}}) where
 	return int * real(field.n)
 end
 
-function lightinteraction_recursivegridded!(fieldl::AbstractFieldMonochromatic{T}, fieldr::AbstractFieldMonochromatic{T}, coefs::AbstractVector{<:AbstractCoefficient{T}}, fieldi::AbstractFieldMonochromatic{T}; rtol = 1E-3one(T)::Real, cval = one(T)::Real) where {T<:Real}
+function lightinteraction_recursivegridded!(fieldl::AbstractFieldMonochromatic{T}, fieldr::AbstractFieldMonochromatic{T}, coefs::AbstractVector{<:AbstractCoefficient{T}}, fieldi::AbstractFieldMonochromatic{T}; rtol = 1E-3one(T)::Real) where {T<:Real}
 	# miss check if this can be done
 	sizeL = length(coefs) + 1;
 
@@ -38,79 +38,74 @@ function lightinteraction_recursivegridded!(fieldl::AbstractFieldMonochromatic{T
 	fieldi.dir > 0 ? vec(fields_r[1].e_SXY) .= vec(fieldi.e_SXY) : vec(fields_l[sizeL].e_SXY) .= vec(fieldi.e_SXY)
 	initial_int = intensity_p(fieldi)
 	fieldi.dir > 0 ? int_r[1] = initial_int : int_l[sizeL] = initial_int
+	fields2_l = deepcopy(fields_l)
+	fields2_r = deepcopy(fields_r)
 
 	i = 1
-	arg_l = 1
-	arg_r = 1
+	toSave_l = fields_l
+	toSave_r = fields_r
 	@inbounds while true
-		# Select the next field to consider
-		max_r = zero(T)
-		max_l = zero(T)
-		for a in 1:sizeL-1
-			if max_r < cval_r[a] * int_r[a]
-				max_r = cval_r[a] * int_r[a]
-				arg_r = a
-			end
-			if max_l < cval_l[a+1] * int_l[a+1]
-				max_l = cval_l[a+1] * int_l[a+1]
-				arg_l = a
-			end
+		if i % 2 == 1
+			toSave_l = fields_l
+			toSave_r = fields_r
+			iE_l = fields2_l
+			iE_r = fields2_r
+		else
+			toSave_l = fields2_l
+			toSave_r = fields2_r
+			iE_l = fields_l
+			iE_r = fields_r
 		end
 
-		if max_r > max_l # Field is propagating forward
-			max_r / cval_r[arg_r] < rtol && break
-			mls = arg_r
-			fields_r[mls].ref == coefs[mls].fieldl.ref || tobedone()
-			lightinteraction!(fields_aux_l[mls], fields_aux_r[mls+1], coefs[mls], fields_r[mls])
-			add_inplace!(fields_l[mls], fields_aux_l[mls])
-			add_inplace!(fields_r[mls+1], fields_aux_r[mls+1])
-			int_l[mls] = intensity_p(fields_l[mls])
-			int_r[mls+1] = intensity_p(fields_r[mls+1])
-			vec(fields_r[mls].e_SXY) .= zero(Complex{T})
-			int_r[mls] = zero(T)
-			cval_r[mls] = one(T)
-		else # field is going backward
-			max_l / cval_l[arg_l + 1] < rtol && break
-			mls = arg_l + 1 # need to add +1 because length start from 2
-			fields_l[mls].ref == coefs[mls-1].fieldr.ref || tobedone()
-			lightinteraction!(fields_aux_l[mls-1], fields_aux_r[mls], coefs[mls-1], fields_l[mls])
-			add_inplace!(fields_l[mls-1], fields_aux_l[mls-1])
-			add_inplace!(fields_r[mls], fields_aux_r[mls])
-			int_l[mls-1] = intensity_p(fields_l[mls-1])
-			int_r[mls] = intensity_p(fields_r[mls])
-			vec(fields_l[mls].e_SXY) .= zero(Complex{T})
-			int_l[mls] = zero(T)
-			cval_l[mls] = one(T)
+		for mls in 1:sizeL
+			if mls < sizeL
+				if int_r[mls] > rtol * 1E-2
+					iE_r[mls].ref == coefs[mls].fieldl.ref || tobedone()
+					lightinteraction!(fields_aux_l[mls], fields_aux_r[mls+1], coefs[mls], iE_r[mls])
+					add_inplace!(toSave_l[mls], fields_aux_l[mls])
+					add_inplace!(toSave_r[mls+1], fields_aux_r[mls+1])
+				else
+					# add_inplace!(toSave_r[mls], iE_r[mls])
+				end
+				vec(iE_r[mls].e_SXY) .= zero(Complex{T})
+			end
+			if mls > 1
+				if int_l[mls] > rtol * 1E-2
+					iE_l[mls].ref == coefs[mls-1].fieldr.ref || tobedone()
+					lightinteraction!(fields_aux_l[mls-1], fields_aux_r[mls], coefs[mls-1], iE_l[mls])
+					add_inplace!(toSave_l[mls-1], fields_aux_l[mls-1])
+					add_inplace!(toSave_r[mls], fields_aux_r[mls])
+				else
+					# add_inplace!(toSave_l[mls], iE_l[mls])
+				end
+				vec(iE_l[mls].e_SXY) .= zero(Complex{T})
+			end
 		end
+		for mls in 1:sizeL
+			int_l[mls] = intensity_p(toSave_l[mls])
+			int_r[mls] = intensity_p(toSave_r[mls])
+		end
+
+		sum(view(int_l,2:sizeL)) + sum(view(int_r, 1:sizeL-1)) < rtol && break
 		sum(int_l) + sum(int_r) > 10initial_int && (println("lightinteraction_recursivegridded is not converging. Change cval (increase is recommended)."); break)
-		i > 1000000 && error("Max number of iterations achieved")
+		i > 10000 && error("Max number of iterations achieved")
 		i += 1
-
-		cval_l .*= cval
-		cval_r .*= cval
-
-		# @show cval_l
-		# @show int_l
-		# @show cval_r
-		# @show int_r
-		# @show initial_int
-
 	end
-	@show i
-	vec(fieldl.e_SXY) .= vec(fields_l[1].e_SXY)
-	vec(fieldr.e_SXY) .= vec(fields_r[sizeL].e_SXY)
+	println("Interactions until convergence: ", i)
+	vec(fieldl.e_SXY) .= vec(toSave_l[1].e_SXY)
+	vec(fieldr.e_SXY) .= vec(toSave_r[sizeL].e_SXY)
 end
 
-function lightinteraction_recursivegridded(coefs::AbstractVector{<:AbstractCoefficient{T}}, field_inc::AbstractFieldMonochromatic{T}; rtol = 1E-3, cval = one(T)) where T
+function lightinteraction_recursivegridded(coefs::AbstractVector{<:AbstractCoefficient{T}}, field_inc::AbstractFieldMonochromatic{T}; rtol = 1E-3) where T
 	(fieldl, tmp) = getfields_lr(coefs[1])
 	(tmp, fieldr) = getfields_lr(coefs[end])
 
 	samedefinitions(field_inc, field_inc.dir > 0 ? fieldl : fieldr) || error("Cannot do this")
-	lightinteraction_recursivegridded!(fieldl, fieldr, coefs, field_inc, rtol = rtol, cval = cval)
+	lightinteraction_recursivegridded!(fieldl, fieldr, coefs, field_inc, rtol = rtol)
 	return (fieldl, fieldr)
 end
 
-function lightinteraction_recursivegridded(comps::AbstractVector{<:AbstractOpticalComponent{T}}, fieldi::AbstractFieldMonochromatic{T}; rtol = 1E-3, cval = one(T)) where T
+function lightinteraction_recursivegridded(comps::AbstractVector{<:AbstractOpticalComponent{T}}, fieldi::AbstractFieldMonochromatic{T}; rtol = 1E-3) where T
 	coefs = coefficient_specific(comps, fieldi)
-	return lightinteraction_recursivegridded(coefs, fieldi, rtol = rtol, cval = cval)
+	return lightinteraction_recursivegridded(coefs, fieldi, rtol = rtol)
 end
