872d203284c59a7ee576de388f41a5fa1371fc60
Dylan Marques <rmapmd1@ucl.ac.uk>
2020-11-23 17:39:47 +0000

    update interpolations and specialFunction. remove expint as it is in specialfunctions

--- src/Jolab.jl
+++ src/Jolab.jl
@@ -56,7 +56,6 @@ module Jolab
 	include("AbstractCoefficient.jl")
 	include("AbstractOpticalComponent.jl")
 	include("AbstractDetector.jl")
-	include("expint.jl")
 
 	include("numericalFunctions.jl");
 	include("JolabFunction.jl")
