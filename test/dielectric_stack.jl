eee202d88a9b1c0c684ae9baf153532bcba07230
DylanMMarques <d.m.marques@utwente.nl>
2026-05-03 14:28:00 +0200

    update dielectric stack tests

--- test/dielectric_stack.jl
+++ test/dielectric_stack.jl
@@ -1,73 +1,50 @@
-using Jolab, StaticArrays, Test, FiniteDiff, Enzyme
-import Jolab: Forward, Backward
-import FiniteDiff: finite_difference_derivative
+using Jolab, StaticArrays, Test
 
 ## Test errors 
 
 stack_test = DielectricStack(Medium.((@SVector [1, 1.5, 1])), (@SVector [100E-9]), ReferenceFrame((0,0,0), (0,0,0)))
 
-pw = MeshedPlaneWaveScalar(Forward, 0, 0.1, 1, 1550E-9, Medium(2.0), ReferenceFrame((1,0,0), (0,0,0)))
+grid_pw = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, 0.0, 0.1, 1550E-9)
+pw = Jolab.AngularSpectrum(Jolab.Forward, grid_pw, reshape([1.0 + 0.0im], 1, 1), Medium(2.0), ReferenceFrame((1,0,0), (0,0,0)))
 @test_throws ArgumentError light_interaction(stack_test, pw)
 
-pw = MeshedPlaneWaveScalar(Forward, 0, 0.1, 1, 1550E-9, Medium(1.0), ReferenceFrame((2,0,0), (0,0,0)))
+grid_pw = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, 0.0, 0.1, 1550E-9)
+pw = Jolab.AngularSpectrum(Jolab.Forward, grid_pw, reshape([1.0 + 0.0im], 1, 1), Medium(1.0), ReferenceFrame((2,0,0), (0,0,0)))
 @test_throws ArgumentError light_interaction(stack_test, pw)
 
-pw = MeshedPlaneWaveScalar(Forward, 0, 0.1, 1, 1550E-9, Medium(1.0), ReferenceFrame((2,0,0), (0,.5,.1)))
+grid_pw = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, 0.0, 0.1, 1550E-9)
+pw = Jolab.AngularSpectrum(Jolab.Forward, grid_pw, reshape([1.0 + 0.0im], 1, 1), Medium(1.0), ReferenceFrame((2,0,0), (0,.5,.1)))
 @test_throws ArgumentError light_interaction(stack_test, pw)
 
-pw = MeshedPlaneWaveScalar(Forward, 0, 0.1, 1, 1550E-9, Medium(1.0), ReferenceFrame((1,0,0), (0,.5,.1)))
+grid_pw = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, 0.0, 0.1, 1550E-9)
+pw = Jolab.AngularSpectrum(Jolab.Forward, grid_pw, reshape([1.0 + 0.0im], 1, 1), Medium(1.0), ReferenceFrame((1,0,0), (0,.5,.1)))
 @test_throws ArgumentError light_interaction(stack_test, pw)
-
-## Test types 
-
-function test_type(T)
-    stack_test = DielectricStack(T, Medium.((@SVector [1, 1.5, 1])), (@SVector [100E-9]), ReferenceFrame((0,0,0), (0,0,0)))
-    pw = MeshedPlaneWaveScalar(T, Forward, 0, 0.1, 1, 1550E-9, Medium(1.0), ReferenceFrame((0,0,0), (0,0,0)))
-    (rpw, tpw) = light_interaction(stack_test, pw)
-    eltype(rpw.e) == Complex{T} && eltype(tpw.e) == Complex{T}
-end
-@test test_type(BigFloat)
-@test test_type(Float64)
-@test test_type(Float32)
-@test test_type(Float16)
-
-function test_type_complex(T)
-    stack_test = DielectricStack(T, Medium.((@SVector [1 + im, 1.5 + im, 1 + im])), (@SVector [100E-9]), ReferenceFrame((0,0,0), (0,0,0)))
-    pw = MeshedPlaneWaveScalar(T, Forward, 0, 0.1, 1, 1550E-9, Medium(1 + im), ReferenceFrame((0,0,0), (0,0,0)))
-    (rpw, tpw) = light_interaction(stack_test, pw)
-    eltype(rpw.e) == Complex{T} && eltype(tpw.e) == Complex{T}
-end
-@test test_type_complex(BigFloat)
-@test test_type_complex(Float64)
-@test test_type_complex(Float32)
-@test test_type_complex(Float16)
-@test_opt test_type_complex(Float64)
-
 ## Test values
 function test_reflection_coeffiecient(nsx, nsy, λ, mls)
-    pw = Jolab.MeshedPlaneWaveScalar(Forward, nsx, nsy, 1, λ, first(mls.mat), ReferenceFrame((0,0,0), (0,0,0)))
+    grid_pw = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, nsx, nsy, λ)
+    pw = Jolab.AngularSpectrum(Jolab.Forward, grid_pw, reshape([1.0 + 0.0im], 1, 1), first(mls.mat), ReferenceFrame((0,0,0), (0,0,0)))
     (rpw, tpw) = light_interaction(mls, pw)
     (rpw.e[1], tpw.e[1])
 end
 ref = ReferenceFrame((0,0,0), (0,0,0))
 stack_test = DielectricStack(Medium.((@SVector [1, 2, 1])), (@SVector [100E-9]), ref)
-@test all(test_reflection_coeffiecient(0, 0, 1550E-9, stack_test) .≈ (-0.3801572124640627 + 0.28909310142234657im, 0.5318174730142268 + 0.6993395798310894im))
-@test all(test_reflection_coeffiecient(0, 0.1, 1550E-9, stack_test) .≈ (-0.3817505426194731 + 0.2902356449628659im, 0.531090735792542 + 0.6985502300894781im))
-@test all(test_reflection_coeffiecient(0.1, 0, 1550E-9, stack_test) .≈ (-0.3817505426194731 + 0.2902356449628659im, 0.531090735792542 + 0.6985502300894781im))
+@test all(test_reflection_coeffiecient(0.0, 0.0, 1550E-9, stack_test) .≈ (-0.3801572124640627 + 0.28909310142234657im, 0.5318174730142268 + 0.6993395798310894im))
+@test all(test_reflection_coeffiecient(0.0, 0.1, 1550E-9, stack_test) .≈ (-0.3817505426194731 + 0.2902356449628659im, 0.531090735792542 + 0.6985502300894781im))
+@test all(test_reflection_coeffiecient(0.1, 0.0, 1550E-9, stack_test) .≈ (-0.3817505426194731 + 0.2902356449628659im, 0.531090735792542 + 0.6985502300894781im))
 @test all(test_reflection_coeffiecient(0.1, 0.1, 1550E-9, stack_test) .≈ (-0.38336673222871664 + 0.29138460541823613im, 0.5303414130920262 + 0.6977556491387847im))
 
 stack_test = DielectricStack(Medium.((@SVector [1, 2 + im, 1])), (@SVector [200E-9]), ref)
-@test all(test_reflection_coeffiecient(0, 0, 1550E-9, stack_test) .≈ (-0.4726748478534631 - 0.2266907860346169im, 0.06294761742913318 + 0.3843200058489115im))
-@test all(test_reflection_coeffiecient(0.1, 0, 1550E-9, stack_test) .≈ (-0.4748859437971371 - 0.22622476562202212im, 0.06387585643028817 + 0.3830637619377823im))
-@test all(test_reflection_coeffiecient(0, 0.1, 1550E-9, stack_test) .≈ (-0.4748859437971371 - 0.22622476562202212im, 0.06387585643028817 + 0.3830637619377823im))
+@test all(test_reflection_coeffiecient(0.0, 0.0, 1550E-9, stack_test) .≈ (-0.4726748478534631 - 0.2266907860346169im, 0.06294761742913318 + 0.3843200058489115im))
+@test all(test_reflection_coeffiecient(0.1, 0.0, 1550E-9, stack_test) .≈ (-0.4748859437971371 - 0.22622476562202212im, 0.06387585643028817 + 0.3830637619377823im))
+@test all(test_reflection_coeffiecient(0.0, 0.1, 1550E-9, stack_test) .≈ (-0.4748859437971371 - 0.22622476562202212im, 0.06387585643028817 + 0.3830637619377823im))
 @test all(test_reflection_coeffiecient(0.1, 0.1, 1550E-9, stack_test) .≈ (-0.4771130976569241 - 0.22574918641516817im, 0.06479985900217344 + 0.38179405532679594im))
 
 stack_test = DielectricStack(Medium.((@SVector [2, 1, 2])), (@SVector [200E-9]), ref)
-@test all(test_reflection_coeffiecient(0, 0, 1550E-9, stack_test) .≈ (0.3801572124640627 - 0.28909310142234657im, 0.5318174730142268 + 0.6993395798310894im)
+@test all(test_reflection_coeffiecient(0.0, 0.0, 1550E-9, stack_test) .≈ (0.3801572124640627 - 0.28909310142234657im, 0.5318174730142268 + 0.6993395798310894im)
 )
-@test all(test_reflection_coeffiecient(0.1, 0, 1550E-9, stack_test) .≈ (0.38004014247437345 - 0.2907048932717478im, 0.5335016668393845 + 0.6974497305293892im)
+@test all(test_reflection_coeffiecient(0.1, 0.0, 1550E-9, stack_test) .≈ (0.38004014247437345 - 0.2907048932717478im, 0.5335016668393845 + 0.6974497305293892im)
 )
-@test all(test_reflection_coeffiecient(0, 0.1, 1550E-9, stack_test) .≈ (0.38004014247437345 - 0.2907048932717478im, 0.5335016668393845 + 0.6974497305293892im)
+@test all(test_reflection_coeffiecient(0.0, 0.1, 1550E-9, stack_test) .≈ (0.38004014247437345 - 0.2907048932717478im, 0.5335016668393845 + 0.6974497305293892im)
 )
 @test all(test_reflection_coeffiecient(0.1, 0.1, 1550E-9, stack_test) .≈ (0.3799169568043824 - 0.2923210356280086im, 0.5351813183829595 + 0.6955519207907793im)
 )
@@ -75,11 +52,11 @@ stack_test = DielectricStack(Medium.((@SVector [2, 1, 2])), (@SVector [200E-9]),
 ## Water to air for total internal reflection testing
 stack_test = DielectricStack(Medium.((@SVector [1.33, 1, 1])), (@SVector Float64[10E-6]), ref)
 
-@test all(.≈(test_reflection_coeffiecient(0, 0.8 * 1.333, 1550E-9, stack_test), (0.6431032383925089 - 0.765779488344437im, 4.949051151775065e-7 - 2.3065390964139887e-7im), atol = 1E-13))
-@test all(.≈(test_reflection_coeffiecient(0, 0.8 * 1.333, 1550E-9, stack_test), (0.6431032383925089 - 0.765779488344437im, 4.949051151775065e-7 - 2.3065390964139887e-7im), atol = 1E-13))
-@test all(.≈(test_reflection_coeffiecient(0.8 * 1.333, 0, 1550E-9, stack_test), (0.6431032383925089 - 0.765779488344437im, 4.949051151775065e-7 - 2.3065390964139887e-7im), atol = 1E-13))
-@test all(.≈(test_reflection_coeffiecient(0.7 * 1.333, 0, 1550E-9, stack_test), (0.4498594097636552 + 0.0im, -0.6182242523375912 + 1.3114461795673662im), atol = 1E-12, rtol = 1E-6))
-@test all(.≈(test_reflection_coeffiecient(0, 0.7 * 1.333, 1550E-9, stack_test), (0.4498594097636552 + 0.0im, -0.6182242523375912 + 1.3114461795673662im), atol = 1E-12, rtol = 1E-6))
+@test all(.≈(test_reflection_coeffiecient(0.0, 0.8 * 1.333, 1550E-9, stack_test), (0.6431032383925089 - 0.765779488344437im, 4.949051151775065e-7 - 2.3065390964139887e-7im), atol = 1E-13))
+@test all(.≈(test_reflection_coeffiecient(0.0, 0.8 * 1.333, 1550E-9, stack_test), (0.6431032383925089 - 0.765779488344437im, 4.949051151775065e-7 - 2.3065390964139887e-7im), atol = 1E-13))
+@test all(.≈(test_reflection_coeffiecient(0.8 * 1.333, 0.0, 1550E-9, stack_test), (0.6431032383925089 - 0.765779488344437im, 4.949051151775065e-7 - 2.3065390964139887e-7im), atol = 1E-13))
+@test all(.≈(test_reflection_coeffiecient(0.7 * 1.333, 0.0, 1550E-9, stack_test), (0.4498594097636552 + 0.0im, -0.6182242523375912 + 1.3114461795673662im), atol = 1E-12, rtol = 1E-6))
+@test all(.≈(test_reflection_coeffiecient(0.0, 0.7 * 1.333, 1550E-9, stack_test), (0.4498594097636552 + 0.0im, -0.6182242523375912 + 1.3114461795673662im), atol = 1E-12, rtol = 1E-6))
 
 ## Test radial with cartesian beam
 stack_test = DielectricStack(Medium.((@SVector [1, 2, 3])), (@SVector Float64[10E-6]), ref)
@@ -89,8 +66,10 @@ stack_test = DielectricStack(Medium.((@SVector [1, 2, 3])), (@SVector Float64[10
 stack_f = DielectricStack(Medium.((@SVector [1, 4, 2+im, 1])), (@SVector [500E-9, 200E-9]), ref)
 stack_b = DielectricStack(Medium.((@SVector [1, 2+im, 4, 1])), (@SVector [200E-9, 500E-9]), ref)
 function test_reflection_coeffiecient(stack_forward, stack_backward, nsx, nsy, λ)
-    pw_f = MeshedPlaneWaveScalar(Forward, nsx, nsy, 1, λ, first(stack_forward.mat), ref)
-    pw_b = MeshedPlaneWaveScalar(Backward, nsx, nsy, 1, λ, first(stack_backward.mat), last(stack_backward.frames))
+    grid_pw_f = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, nsx, nsy, λ)
+    pw_f = Jolab.AngularSpectrum(Jolab.Forward, grid_pw_f, reshape([1.0 + 0.0im], 1, 1), first(stack_forward.mat), ref)
+    grid_pw_b = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, nsx, nsy, λ)
+    pw_b = Jolab.AngularSpectrum(Jolab.Backward, grid_pw_b, reshape([1.0 + 0.0im], 1, 1), first(stack_backward.mat), last(stack_backward.frames))
     (rpw_b, tpw_b) = light_interaction(stack_backward, pw_b)
     (rpw_f, tpw_f) = light_interaction(stack_forward, pw_f)
     rpw_f.e[1] ≈ tpw_b.e[1] && tpw_f.e[1] ≈ rpw_b.e[1]
@@ -99,15 +78,17 @@ end
 
 function f_beam(λ)
     mls = DielectricStack(Medium.((@SVector [1, 1.5, 1])), (@SVector [100E-9]), ReferenceFrame((0,0,0), (0,0,1)))
-    nsx = range(-.95, 0.95, length = 100)
-    beam = MonochromaticAngularSpectrum(Forward, nsx, nsx, (nsx .* nsx'), λ, Medium(1.0), ReferenceFrame((0,0,0), (0,0,1)));
+    nsx = range(-.95, 0.95, length = 50)
+    grid_ang = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, nsx, nsx, λ)
+    beam = Jolab.AngularSpectrum(Jolab.Forward, grid_ang, reshape(nsx .* nsx', (length(nsx), length(nsx))), Medium(1.0), ReferenceFrame((0,0,0), (0,0,1)))
     (rbeam, tbeam) = light_interaction(mls, beam)
 end
 @test_opt f_beam(1550E-9)
 
 function test_scatmat_f(mls)
-    nsx = range(0, 0.95, length = 100)
-    beam = MonochromaticAngularSpectrum(Float64, Forward, nsx, nsx, (nsx .* nsx'), 1500E-9, Medium(1.0), ReferenceFrame((0,0,0), (0,0,0)));
+    nsx = range(0, 0.95, length = 50)
+    grid_ang = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, nsx, nsx, 1500E-9)
+    beam = Jolab.AngularSpectrum(Float64, Jolab.Forward, grid_ang, reshape(nsx .* nsx', (length(nsx), length(nsx))), Medium(1.0), ReferenceFrame((0,0,0), (0,0,0)))
     mat = ScatteringMatrix(mls, beam)
     (mat_r, mat_t) = light_interaction(mat, beam)
     (aux_r, aux_t) = light_interaction(mls, beam)
@@ -120,8 +101,9 @@ mirror = Mirror((Medium(1.0), Medium(1.0)), ReferenceFrame((0,0,0), (0,0,0)), re
 @test test_scatmat_f(mirror)
 
 function test_scatmat_b(mls)
-    nsx = range(0, 0.95, length = 100)
-    beam = MonochromaticAngularSpectrum(Float64, Backward, nsx, nsx, (nsx .* nsx'), 1550E-9, Medium(1.0), last(mls.frames));
+    nsx = range(0, 0.95, length = 50)
+    grid_ang = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, nsx, nsx, 1550E-9)
+    beam = Jolab.AngularSpectrum(Float64, Jolab.Backward, grid_ang, reshape(nsx .* nsx', (length(nsx), length(nsx))), Medium(1.0), last(mls.frames))
     mat = ScatteringMatrix(mls, beam)
     (mat_r, mat_t) = mat * beam
     (aux_r, aux_t) = light_interaction(mls, beam)
@@ -130,121 +112,12 @@ end
 @test test_scatmat_b(mls)
 @test test_scatmat_b(mirror)
 
-nsr = range(0, 0.5, length = 1000)
-field = MonochromaticAngularSpectrumRadialSymmetric_gaussian(Forward, nsr, 10E-6, 1550E-9, Medium(1), ReferenceFrame((0,0,0), (0,0,0)))
+nsr = range(0, 0.5, length = 50)
+nθ = 0.0
+λ = 1550E-9
+grid_field = Jolab.CylindricalGrid(Jolab.NSR_NSθ_λ, nsr, nθ, λ)
+field = Jolab.AngularSpectrum(Jolab.Forward, grid_field, Jolab.Gaussian(10E-6), Medium(1), ReferenceFrame((0,0,0), (0,0,0)))
 mls = DielectricStack(Medium.((@SVector [1, 1.5, 1])), (@SVector [100E-9]), ReferenceFrame((0,0,100E-9), (0,0,0)))
 @test_throws ArgumentError light_interaction(mls, field)
 @test_throws ArgumentError ScatteringMatrix(mls, field)
 
-## Jacobian tests
-function jac(x)
-    n, k, h, nsx, nsy, λ = x
-    stack_test = DielectricStack(Float64, Medium.((@SVector [1 + k*im, n, 1])), (@SVector [h]), ReferenceFrame((0,0,0), (0,0,1)))
-    Jolab.rtss(stack_test, Forward, sqrt(nsx^2 + nsy^2), λ)
-end
-enz_jac = Enzyme.jacobian(Enzyme.Forward, jac, [2, 1, 100E-9, .1, .1, 1550E-9])
-
-fin_jac_r = FiniteDiff.finite_difference_jacobian(x -> jac(x)[1], [2, 1, 100E-9, .1, .1, 1550E-9], Val{:central}, ComplexF64, relstep = 1E-9)
-fin_jac_t = FiniteDiff.finite_difference_jacobian(x -> jac(x)[2], [2, 1, 100E-9, .1, .1, 1550E-9], Val{:central}, ComplexF64, relstep = 1E-9)
-
-@test all(isapprox.(first.(enz_jac[1]), vec(fin_jac_r), rtol = 1E-4))
-@test all(isapprox.(last.(enz_jac[1]), vec(fin_jac_t), rtol = 1E-4))
-
-## Plane Wave tests
-# Wavelength dependency
-
-stack_test = DielectricStack(Medium.((@SVector [1+0im, 2.0, 1])), (@SVector [100E-9]), ReferenceFrame((0,0,0), (0,0,1)))
-
-ad_diff = autodiff(Enzyme.Forward, Jolab.rtss, Duplicated, Const(stack_test), Const(Forward), Const(0.1), Duplicated(1550E-9, 1.0))[1]
-num_diff = (finite_difference_derivative(i -> Jolab.rtss(stack_test, Forward, 0.1, i)[1], 1550E-9; absstep = 1E-18), finite_difference_derivative(i -> Jolab.rtss(stack_test, Forward, 0.1, i)[2], 1550E-9; absstep = 1E-18))
-@test all((num_diff) .≈ ad_diff)
-
-# direction dependency
-ad_diff = autodiff(Enzyme.Forward, Jolab.rtss, Duplicated, Const(stack_test), Const(Forward), Duplicated(0.1, 1.0), Const(1550E-9))[1]
-num_diff = (finite_difference_derivative(i -> Jolab.rtss(stack_test, Forward, i, 1550E-9)[1], 0.1; absstep = 1E-18),
-    finite_difference_derivative(i -> Jolab.rtss(stack_test, Forward, i, 1550E-9)[2], 0.1; absstep = 1E-10))
-@test all((num_diff) .≈ ad_diff)
-
-# Refractive index dependency
-function rtss_f(n)
-    mls = DielectricStack(Medium.((@SVector [1+0im, n, 1])), (@SVector [100E-9]), ReferenceFrame((0,0,0), (0,0,1)))
-    Jolab.rtss(mls, Forward, 0.1, 1550E-9)
-end
-ad_diff = autodiff(Enzyme.Forward, rtss_f, Duplicated, Duplicated(2.0, 1.0))[1]
-num_diff = (finite_difference_derivative(i -> rtss_f(i)[1], 2.0; absstep = 1E-18),
-    finite_difference_derivative(i -> rtss_f(i)[2], 2.0; absstep = 1E-18))
-@test all(ad_diff .≈ num_diff)
-
-# Thickness dependency
-function thickness_dependency(h)
-    mls = DielectricStack(Medium.((@SVector [1+0im, 2, 1])), (@SVector [h]), ReferenceFrame((0,0,0), (0,0,1)))
-    Jolab.rtss(mls, Forward, 0.1, 1550E-9)
-end
-ad_diff = autodiff(Enzyme.Forward, thickness_dependency, Duplicated, Duplicated(10E-9, 1.0))[1]
-num_diff = (finite_difference_derivative(i -> thickness_dependency(i)[1], 10E-9; absstep = 1E-20),
-    finite_difference_derivative(i -> thickness_dependency(i)[2], 10E-9; absstep = 1E-20))
-@test all(ad_diff .≈ num_diff)
-
-
-function f_rbeam(mls, nsx, nsy, waist, λ, medium, frame)
-    grid_ang = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, nsx, nsy, λ)
-    beam = Jolab.AngularSpectrum(Float64, Forward, grid_ang, Jolab.Gaussian(waist), medium, frame)
-    (rbeam, tbeam) = light_interaction(mls, beam)
-    intensity(rbeam)
-end
-
-function f_tbeam(mls, nsx, nsy, waist, λ, medium, frame)
-    grid_ang = Jolab.CartesianGrid(Jolab.NSX_NSY_λ, nsx, nsy, λ)
-    beam = Jolab.AngularSpectrum(Float64, Forward, grid_ang, Jolab.Gaussian(waist), medium, frame)
-    (rbeam, tbeam) = light_interaction(mls, beam)
-    intensity(tbeam)
-end
-
-medium = Medium(1.0)
-frame = ReferenceFrame((0,0,0), (0,0,1))
-nsx = range(-0.5, 0.5, length = 100)
-nsy = nsx
-waist = 10E-6
-T = Float64
-mls = DielectricStack(Medium.((@SVector [1, 1.5, 1])), (@SVector [100E-9]), ReferenceFrame((0,0,0), (0,0,1)))
-
-ar(λ) = f_rbeam(mls, nsx, nsy, waist, λ * 1E-9, medium, frame)
-at(λ) = f_tbeam(mls, nsx, nsy, waist, λ * 1E-9, medium, frame)
-
-num_diff = finite_difference_derivative(ar, 1550.0; absstep = 1E-50)
-val = ar(1550.0)
-ad_diff = Tuple(autodiff(set_runtime_activity(Enzyme.ForwardWithPrimal), ar, Duplicated, Duplicated(1550.0, 1.0)))
-@test all((ad_diff) .≈ (num_diff, val))
-
-num_diff = finite_difference_derivative(at, 1550.0; absstep = 1E-20)
-val = at(1550.0)
-ad_diff = Tuple(autodiff(set_runtime_activity(Enzyme.ForwardWithPrimal), at, Duplicated, Duplicated(1550.0, 1.0)))
-@test all((ad_diff) .≈ (num_diff, val))
-
-# Broken tests
-# mirror = Mirror((Medium(1.0), Medium(1.0)), ReferenceFrame((0,0,0), (0,0,0)); reflectivity = 0.99)
-# ar(λ) = f_rbeam(mirror, nsx, nsy, e, medium, frame, λ)
-# at(λ) = f_tbeam(mirror, nsx, nsy, e, medium, frame, λ)
-#
-# num_diff = finite_difference_derivative(i -> f_rbeam(mirror, nsx, nsy, e, medium, frame, i), 1550E-9; absstep = 1E-20)
-# val = f_rbeam(mirror, nsx, nsy, e, medium, frame, 1550E-9)
-# ad_diff = Tuple(autodiff(Enzyme.Forward, ar, Duplicated, Duplicated(1550E-9, 1.0)))
-# @test all((ad_diff) .≈ (val, num_diff))
-#
-# num_diff = finite_difference_derivative(i -> f_tbeam(mirror, nsx, nsy, e, medium, frame, i), 1550E-9; absstep = 1E-20)
-# val = f_tbeam(mirror, nsx, nsy, e, medium, frame, 1550E-9)
-# ad_diff = Tuple(autodiff(Enzyme.Forward, at, Duplicated, Duplicated(1550E-9, 1.0)))
-# @test all((ad_diff) .≈ (val, num_diff))
-#
-#
-# ## Auto diff on Radial symmetric beams
-# function f_beam(mls, nsr, ω, medium, frame, λ)
-#     beam2 = MonochromaticAngularSpectrumRadialSymmetric_gaussian(Float64, Forward, nsr, ω, λ, medium, frame)
-#     (rbeam, tbeam) = light_interaction(mls, beam2)
-#     (intensity(tbeam), intensity(rbeam))
-# end
-# aux_f(λ) = f_beam(mls, nsx, 10E-6, medium, frame, λ)
-# ad_diff = autodiff(Enzyme.Forward, aux_f, Duplicated, Duplicated(1550E-9, 1.0))
-# num_diff_t = finite_difference_derivative(i -> aux_f(i)[1], 1550E-9; absstep = 1E-20)
-# num_diff_r = finite_difference_derivative(i -> aux_f(i)[2], 1550E-9; absstep = 1E-20)
-# @test all(ad_diff[1] .≈ (num_diff_t, num_diff_r))
